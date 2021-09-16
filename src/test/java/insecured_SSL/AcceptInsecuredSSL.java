7e2b8ead10c2d3cc814005908d204721fbd03844
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2021-09-16 13:43:52 +0600

    Initial commit

---
+++ src/test/java/insecured_SSL/AcceptInsecuredSSL.java
@@ -0,0 +1,35 @@
+package insecured_SSL;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import base_class.DriverSetup;
+
+public class AcceptInsecuredSSL extends DriverSetup {
+	@Test
+	public void InsecuredSSL() {
+// TODO Auto-generated method stub
+
+//SSl certificates
+
+//Desired capabilities=
+//general chrome profile
+		DesiredCapabilities ch = DesiredCapabilities.chrome();
+//ch.acceptInsecureCerts();
+		ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+//Belows to your local browser
+		ChromeOptions c = new ChromeOptions();
+		c.merge(ch);
+		System.setProperty("webdriver.chrome.driver", "");
+		WebDriver driver = new ChromeDriver(c);
+
+	}
+
+}
